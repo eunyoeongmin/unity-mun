8d0957a4d65df76355fbaef701ccd08c6e2befb5
EUN YEONG MIN <qlafqjsgh123@naver.com>
2024-09-23 20:59:39 +0900

    0923.2
    dd

--- Assets/MyBall.cs
+++ Assets/MyBall.cs
@@ -9,26 +9,35 @@ public class MyBall : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
         //#1. 속력 바꾸기
         //rigid.velocity = Vertor3.forward;
 
         //#2. 힘 가하기 
-        if(Input.GetButtonDown("Jump")) {
+        /* if(Input.GetButtonDown("Jump")) {
             rigid.AddForce(Vector3.up * 10, ForceMode.Impulse);
         }
         Vector3 vec = new Vector3(
             Input.GetAxisRaw("Horizontal"),
             0,
             Input.GetAxisRaw("Vertical")   
-        );
+        ); */
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 vec = new Vector3(h,0,v);
+
 
         rigid.AddForce(vec, ForceMode.Impulse);
 
         //#3. 회전력 
         // rigid.AddTorque(Vector3.down);
     }
+
+    //트리거 콜라이더(물리x)가 계속 충돌하고 있을 때 호출.
+    private void OnTriggerStay(Collider other) {
+            if(other.name == "new cube")
+                rigid.AddForce(Vector3.up * 2, ForceMode.Impulse);
+        }
 }
--- Assets/otherball.cs
+++ Assets/otherball.cs
@@ -12,10 +12,14 @@ public class otherball : MonoBehaviour
         mat = mesh.material;
     }
 
-    //1.물리적 충돌이 시작할 때 호출되는 함수 
+    //1.콜라이션(물리적 충돌)이 시작할 때 호출되는 함수 
     private void OnCollisionEnter(Collision collision) {
-        if(collision.gameObject.name == "MyBall")
+        if(collision.gameObject.name == "My Ball")
         mat.color = new Color(0,0,0);
+    }
+    private void OnCollisionExit(Collision collision) {
+        if(collision.gameObject.name == "My Ball")
+        mat.color = new Color(1,1,1);
     }
     //2.물리적 충돌중 일 때 호출되는 함수
     /* private void OnCollisionStay(Collision collision) {
